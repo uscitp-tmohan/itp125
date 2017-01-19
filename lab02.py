96a0e3e7cb55663a6fbe249b7a4857fd13177ad5
Tanuja Mohan <tanujamohan@guest-wireless-upc-1602-10-120-047-223.usc.edu>
2017-01-19 10:26:45 -0800

    lab 2 completed

---
+++ lab02.py
@@ -0,0 +1,10 @@
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if not False and 5 > 2:    # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+        return True
+    elif False:
+        return False
+        # Keep going here.
+        # You'll want to add the else statement, too!
